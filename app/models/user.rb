e313a7029357eed384a2867b1ae78aed5a1076d9
Iyadb Horani <i3d@ironic3d.com>
2014-04-26 08:59:54 +0000

    Updating simple forms on sign in and sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
- has_many :statuses, dependent: :destroy
+	has_many :statuses, dependent: :destroy
 
- validates :first_name, presence: true
- validates :last_name, presence: true
- validates :profile_name, presence: true
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :profile_name, presence: true
+	validates :email, presence: true
+	validates :password, presence: true
+	validates :password_confirmation, presence: true
+
+	def full_name
+		first_name + " " + last_name
+	end
 end
